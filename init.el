ac619ab09b3f4d2431ea810e2e0e58ca8d08eaed
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2025-01-02 19:23:38 +0100

    fix: update commands for switching active splitted window

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; Rebind Ctrl+x p to move to the previous window
-(global-set-key (kbd "C-x p") #'previous-multiframe-window)
+(global-set-key (kbd "C-x o") #'previous-multiframe-window)
 
 ;; Ensure Ctrl+x o moves to the next window (default behavior)
-(global-set-key (kbd "C-x o") #'other-window)
+(global-set-key (kbd "C-x p") #'other-window)
