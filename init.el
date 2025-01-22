07d03059502b979df3bd2e9a129f3a1524429719
Simon Ljungbeck <simon.ljungbeck@gmail.com>
2025-01-22 14:42:36 +0100

    feat: bind ctrl+z to undo

--- init.el
+++ init.el
@@ -3,3 +3,9 @@
 
 ;; Ensure Ctrl+x o moves to the next window (default behavior)
 (global-set-key (kbd "C-x p") #'other-window)
+
+;; Make Ctrl+z perform undo
+(global-set-key (kbd "C-z") 'undo)
+
+;; Bind Ctrl+x Ctrl+z to suspend-frame
+(global-set-key (kbd "C-x C-z") 'suspend-frame)
